e51de82347bd89917de0df8991fd3c553076f9f5
raguiguemalak <malakraguigue@gmail.com>
2026-07-21 12:37:24 +0100

    modification de la verification dans le middleware

--- sales-dashboard-backend/middleware/authenticate.middleware.js
+++ sales-dashboard-backend/middleware/authenticate.middleware.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken')
 function authenticate(req, res, next) {
- const token = req.cookies.JWT_ACCESS_SECRET
+ const token = req.cookies.accessToken
 
   if (!token) {
     return res.status(401).json({ error: 'Non authentifié' })
   }
   try{
-   const decoded=jwt.verify(token,process.env.accessToken)
+   const decoded=jwt.verify(token,process.env.JWT_ACCESS_SECRET)
    req.user=decoded // on attache les infos décodées (userId, role) à req.user
                  // pour les rendre disponibles dans les middlewares/controllers suivants
    next()
